4e04882729f9d0f76d1e14a8ee11bf61bf3922e2
reparinhos <tabcarloscasa@gmail.com>
2026-08-03 20:48:29 -0300

    Update script.js
    Add firebase

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// Importando as ferramentas do Firebase via CDN (compatível com GitHub Pages)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// SUAS CHAVE DO PROJETO REPARINHOS
+const firebaseConfig = {
+  apiKey: "AIzaSyD3Un6TzwRP_uCMbH_5Icu48ayJpdrifKY",
+  authDomain: "reparinhos.firebaseapp.com",
+  projectId: "reparinhos",
+  storageBucket: "reparinhos.firebasestorage.app",
+  messagingSenderId: "723417655069",
+  appId: "1:723417655069:web:475bba950ec23453bd23f4"
+};
+
+// Inicializando o Firebase, Autenticação e Banco de Dados
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Lógica de navegação entre telas do app
+document.addEventListener("DOMContentLoaded", () => {
+    const navItems = document.querySelectorAll('.nav-item');
+    const views = document.querySelectorAll('.app-content');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            navItems.forEach(nav => nav.classList.remove('active'));
+            item.classList.add('active');
+
+            const targetViewId = item.getAttribute('data-target');
+
+            views.forEach(view => {
+                view.classList.remove('active');
+            });
+
+            const targetView = document.getElementById(targetViewId);
+            if (targetView) {
+                targetView.classList.add('active');
+            }
+        });
+    });
+});
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     // Seleciona todos os botões da navegação inferior
     const navItems = document.querySelectorAll('.nav-item');
